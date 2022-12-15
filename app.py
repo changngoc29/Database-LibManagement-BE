1faf53ca0b3fa9cc864f1928da59ee3a4b28f99b
changngoc29 <vtkienn@gmail.com>
2022-12-15 09:58:57 +0700

    cors update

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, jsonify
 from routes.books import books
 from routes.customers import customers
 from routes.staffs import staffs
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(books)
 app.register_blueprint(customers)
